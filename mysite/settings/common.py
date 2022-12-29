2a69e85b560f83a54b315c93264394a2ad2d9713
hjkang7096 <hjkang7096@naver.com>
2022-12-29 22:05:10 +0900

    "mysite.settings 변경"

--- mysite/settings/common.py
+++ mysite/settings/common.py
@@ -35,12 +35,9 @@ SECRET_KEY = os.environ.get(
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = int(os.environ.get("DEBUG", 1))
+DEBUG = True
 
-if os.environ.get("DJANGO_ALLOWED_HOSTS"):
-    ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
-else:
-    ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["3.37.167.175"]
 
 
 # Application definition
--- mysite/settings/dev.py
+++ mysite/settings/dev.py
@@ -1,3 +1,2 @@
 from .common import *
 
-ALLOWED_HOSTS = []
