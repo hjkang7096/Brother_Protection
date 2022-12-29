33d41731f4e7acb3624b8fded8ec9c44134c524a
hjkang7096 <hjkang7096@naver.com>
2022-12-29 16:36:01 +0900

    "ALLOWED_HOSTS 변경"

--- mysite/settings/common.py
+++ mysite/settings/common.py
@@ -40,7 +40,7 @@ DEBUG = int(os.environ.get("DEBUG", 1))
 if os.environ.get("DJANGO_ALLOWED_HOSTS"):
     ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 else:
-    ALLOWED_HOSTS = []
+    ALLOWED_HOSTS = ["3.37.167.175"]
 
 
 # Application definition
